1c8b6f2b0bbcb75c3adae32bc98140df3f56f44c
Julien Doiron <judoiron003@rogers.com>
2023-03-03 23:31:51 -0400

    Updated Golang Presentation (References), Added http/json Demo

---
+++ http/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	// if r.URL.Path != "/" {
+	// 	http.Error(w, "404 not found.", http.StatusNotFound)
+	// 	return
+	// }
+
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "add.html")
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		//fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+
+		p := Person{
+			Name: name,
+			Age:  age,
+		}
+
+		// Read the JSON
+		readFile, _ := os.Open("people.json")
+		defer readFile.Close()
+		var existingPeople []Person
+		bytes, _ := io.ReadAll(readFile)
+		json.Unmarshal(bytes, &existingPeople)
+		people := append(existingPeople, p)
+
+		// Write to the JSON
+		writeFile, _ := os.Create("people.json")
+		defer writeFile.Close()
+		jsonData, _ := json.MarshalIndent(people, "", "  ")
+		writeFile.Write(jsonData)
+		handleList(w, r)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func handleList(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "list.html")
+}
+
+func main() {
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/add", handleAdd)
+	http.HandleFunc("/list", handleList)
+
+	fmt.Printf("Starting server for testing HTTP POST...\n")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
